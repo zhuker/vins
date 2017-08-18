e927aeada282eeeb9fa6d34839cafc0a85dc3c5d
php3ik <oles.petriv@gmail.com>
2017-08-18 15:17:57 +0300

    stable timecode OCR training code and model

--- nn/timecode_ocr_model.py
+++ nn/timecode_ocr_model.py
@@ -1,8 +1,8 @@
-from keras.layers import Input, Reshape, Dense, Deconv2D, Activation,concatenate,Convolution2D,Conv2D, Conv1D, LSTM, Bidirectional, Dropout, BatchNormalization, MaxPooling2D
+from keras.layers import Input, Reshape,RepeatVector,Flatten, Dense, Deconv2D, Activation,concatenate,Convolution2D,Conv2D, Conv1D, LSTM, Bidirectional, Dropout, BatchNormalization, MaxPooling2D
 from keras.models import Model
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.wrappers import TimeDistributed
-
+import keras.backend as K
 
 def timecode_ocr_model():
 
@@ -86,4 +86,82 @@ def timecode_ocr_model_small():
 
     return model
 
-timecode_ocr_model_small().summary()
+def timecode_ocr_model_RGB():
+
+    inp = Input((32, 32*16, 3))
+
+    c1 = LeakyReLU(alpha=0.2)(BatchNormalization()(Convolution2D(32, kernel_size=3, padding='valid')(inp)))
+    m1 = MaxPooling2D((2, 2))(c1)
+
+    c2 = LeakyReLU(alpha=0.2)(BatchNormalization()(Convolution2D(48, kernel_size=3, padding='valid')(m1)))
+    m2 = MaxPooling2D((1, 2))(c2)
+
+    c3 = LeakyReLU(alpha=0.2)(BatchNormalization()(Convolution2D(64, kernel_size=3, padding='valid')(m2)))
+    m3 = MaxPooling2D((1, 2))(c3)
+
+    c4 = LeakyReLU(alpha=0.2)(BatchNormalization()(Convolution2D(96, kernel_size=3, padding='valid')(m3)))
+
+    c5 = LeakyReLU(alpha=0.2)(BatchNormalization()(Convolution2D(128, kernel_size=(5,3), padding='valid')(c4)))
+
+    c7 = LeakyReLU(alpha=0.2)(BatchNormalization()(Convolution2D(128, kernel_size=(5,3), padding='valid')(c5)))
+    resh = Reshape((56, 128))(c7)
+    drop = Dropout(0.25)(resh)
+    emb = Bidirectional(LSTM(64, return_sequences=False))(drop)
+    dec = Bidirectional(LSTM(64, return_sequences=True))(RepeatVector(11)(emb))
+    resh2 = Reshape((1, 11, 128))(dec)
+    out = Convolution2D(12, kernel_size=1, padding='same')(resh2)
+    out = Reshape((11, 12))(out)
+    out = Activation('softmax')(out)
+
+    model = Model(input=inp, output=out)
+    model.compile('adam', 'categorical_crossentropy')
+    # model.summary()
+
+    return model
+
+
+
+def timecode_ocr_model_RGB_full():
+
+    inp = Input((27, 27*7, 3))
+
+    c1 = LeakyReLU(alpha=0.2)(BatchNormalization()(Convolution2D(64, kernel_size=3, padding='same', trainable=False)(inp)))
+    c1 = LeakyReLU(alpha=0.2)(BatchNormalization()(Convolution2D(64, kernel_size=3, padding='same', trainable=False)(c1)))
+    m1 = MaxPooling2D((2, 2), trainable=False)(c1)
+
+    c2 = LeakyReLU(alpha=0.2)(BatchNormalization()(Convolution2D(96, kernel_size=3, padding='same', trainable=False)(m1)))
+    c2 = LeakyReLU(alpha=0.2)(BatchNormalization()(Convolution2D(96, kernel_size=3, padding='same', trainable=False)(c2)))
+    m2 = MaxPooling2D((2, 2), trainable=False)(c2)
+
+    c3 = LeakyReLU(alpha=0.2)(BatchNormalization()(Convolution2D(128, kernel_size=3, padding='same', trainable=False)(m2)))
+    m3 = MaxPooling2D((2, 2), trainable=False)(c3)
+
+    c4 = LeakyReLU(alpha=0.2)(BatchNormalization()(Convolution2D(128, kernel_size=(3,1), padding='valid', trainable=False)(m3)))
+
+    resh = Reshape((23, 128), trainable=False)(c4)
+    drop = Dropout(0.25)(resh)
+    emb = Bidirectional(LSTM(64, return_sequences=False, trainable=False))(drop)
+    dec = Bidirectional(LSTM(64, return_sequences=True, trainable=False))(RepeatVector(11, trainable=False)(emb))
+    resh2 = Reshape((1, 11, 128), trainable=False)(dec)
+    out = Convolution2D(12, kernel_size=1, padding='same', trainable=False)(resh2)
+    out = Reshape((11, 12), trainable=False)(out)
+    out = Activation('softmax', name='chars', trainable=False)(out)
+
+    emb_istimecode = Dense(64, activation='relu')(emb)
+    isTimecode = Dense(1, activation='sigmoid', name='conf')(emb_istimecode)
+
+
+    model = Model(input=inp, output=[out, isTimecode])
+    model.compile('adam', ['categorical_crossentropy', 'binary_crossentropy'], loss_weights=[0.,1.])
+    model.summary()
+
+    return model
+
+
+# m = timecode_ocr_model_RGB_full()
+#
+# m.load_weights('checkpoints/timecode_ocr_model_RGB_full_vl0.0328.hdf5')
+# m.save('/home/oles/Vin_ocr/vins/JS/weights/OCRmodel.h5')
+# m.save_weights('/home/oles/Vin_ocr/vins/JS/weights/OCRmodel_weights.hdf5')
+# with open('/home/oles/Vin_ocr/vins/JS/weights/OCRmodel.json', 'w') as f:
+#     f.write(m.to_json())
--- nn/train_timecode_pure_ocr.py
+++ nn/train_timecode_pure_ocr.py
@@ -4,20 +4,21 @@ import random
 import os
 from PIL import Image, ImageFont, ImageDraw
 from scipy.ndimage.filters import gaussian_filter
-from timecode_ocr_model import timecode_ocr_model_small
+from timecode_ocr_model import timecode_ocr_model_RGB_full
 from utils import generate_timecode
 from multiprocessing import cpu_count
 from multiprocessing.pool import ThreadPool
 from scipy.misc import imsave
-
-
+import cv2
+from utils import generate_timecode
 vocabulary = '0123456789:;'
+randomVocab = ' 1234567890ABCDEFGHJKLMNPRSTUVWXYZabcderfgklmnopqrstuv           '
 
-im_height = 32
-im_width = 32*11
+im_height = 8 * 40
+im_width = 16 * 40
 nb_epoch = 100
-BATCH_SIZE = 64
-
+BATCH_SIZE = 16
+extra_pixels = 5
 tc_len = 11
 
 y_coef = 0.1  # position of timecode from top or bottom
@@ -27,7 +28,9 @@ rand_transform = 5
 
 # random backgrounds
 bcgs = []
-bcgs_path = '../../SUN_bcgs/'
+
+bcgs_path = '../../bcgs/'
+bcgs_path = '/DATA/MASK/data/coco/train2014/'
 
 if True:
     bcgs = [os.path.join(root, f) for root, _, files in os.walk(bcgs_path) for f in files if f.endswith('.jpg')]
@@ -42,6 +45,16 @@ else:
 
 fonts = [os.path.join(root, f) for root, _, files in os.walk('monospaced/') for f in files if f.endswith('.ttf')]
 
+def cropBBox( img,bbox):
+    tl_x = max(int((bbox[0]-(bbox[2]/2))*img.shape[1]),0)
+    tl_y = max(int((bbox[1]-(bbox[3]/2))*img.shape[0]),0)
+    br_x = int((bbox[0]+(bbox[2]/2))*img.shape[1])
+    br_y = int((bbox[1]+(bbox[3]/2))*img.shape[0])
+
+    crop = img[tl_y:br_y,tl_x:br_x]
+    crop = cv2.resize(crop,(27*7,27), interpolation=cv2.INTER_CUBIC)
+    return crop
+
 
 def process(z):
     flips = [
@@ -52,66 +65,97 @@ def process(z):
         Image.ROTATE_270,
         Image.TRANSPOSE
     ]
-    transforms = [
-        [Image.EXTENT, 4],
-        [Image.AFFINE, 6],
-        [Image.PERSPECTIVE, 8],
-        [Image.QUAD, 8]
-    ]
 
     while True:
         tc = generate_timecode()
         ff = random.choice(fonts)
 
         try:
-            font = ImageFont.truetype(ff, random.randint(16, 128))
-            bcg_img = Image.open(random.choice(bcgs)).convert('L')
+            font = ImageFont.truetype(ff, random.randint(9, 18))
         except:
             continue
 
         t_width, t_height = font.getsize(tc)
 
-        # final size
-        txt_width = t_width + random.randint(min_padding, max_padding)
-        txt_height = t_height + random.randint(min_padding, max_padding)
+        if t_width < im_width and t_height < im_height * y_coef:
+            try:
+                bcg_img = Image.open(random.choice(bcgs)).convert('RGB')
+            except:
+                continue
 
-        # random bcg transforms
-        if random.uniform(0, 1) > 0.5:
-            bcg_img = bcg_img.transpose(random.choice(flips))
+            x = random.randint(0, im_width - t_width)
+            if random.uniform(0, 1) > 0.5:
+                y = random.randint(0, im_height * y_coef - t_height)
+            else:
+                y = random.randint(im_height * (1 - y_coef), im_height - t_height)
 
-        # t = random.choice(transforms)
-        # bcg_img = bcg_img.transform(
-        #    (txt_width, txt_height), t[0], rand_transform * np.random.rand(t[1]))
+            bcg_img = bcg_img.rotate(random.uniform(0, 360))
 
-        bcg_img = bcg_img.resize((txt_width, txt_height))
+            # random nuisance text
+            for i in range(random.randint(0, 15)):
+                txt = ''.join([random.choice(randomVocab) for i in range(0,random.randint(4,19))])
+                tx = random.randint(0, im_width)
+                ty = random.randint(0, im_height)
+                ImageDraw.Draw(bcg_img).text((tx, ty), txt, font=font, fill=(random.randint(180, 255),random.randint(180, 255),random.randint(180, 255)))
 
-        color = random.randint(0, 255)
+            # random background transform
+            if random.uniform(0, 1) > 0.5:
+                bcg_img = bcg_img.transpose(random.choice(flips))
+            bcg_img = bcg_img.resize((im_width, im_height))
 
-        x = random.randint(0, abs(txt_width - t_width))
-        y = random.randint(0, abs(txt_height - t_height))
+            color = random.randint(150, 255)
 
-        # random black rect
-        if random.uniform(0, 1) > 0.3:
-            ImageDraw.Draw(bcg_img).rectangle([x, y, x + t_width, y + t_height], fill=0)
-            color = random.randint(128, 255)
+            # random black rect
+            if random.uniform(0, 1) > 0.6:
+                ImageDraw.Draw(bcg_img).rectangle(
+                    [x - extra_pixels, y - extra_pixels, x + t_width + extra_pixels, y + t_height + extra_pixels],
+                    fill=0)
+                color = random.randint(150, 255)
 
-        # time code text
-        ImageDraw.Draw(bcg_img).text((x, y), tc, font=font, fill=color)
+            if random.uniform(0, 1) < 0.3:
+                ImageDraw.Draw(bcg_img).rectangle(
+                    [0, 0, im_width, im_height*random.uniform(0.01, 0.2)], fill=0)
+                ImageDraw.Draw(bcg_img).rectangle(
+                    [0, im_height * random.uniform(0.8, 0.99), im_width, im_height], fill=0)
 
-        bcg_img = bcg_img.resize((im_width, im_height), Image.ANTIALIAS)
+            # time code text
+            ImageDraw.Draw(bcg_img).text((x, y), tc, font=font, fill=(color,color,color))
 
-        sigma = random.uniform(0, 2)
-        bcg_img = gaussian_filter(bcg_img, sigma)
+            # bbox = [float(x + t_width/2)/im_width,
+            #         float(y + t_height*1.15/2)/im_height,
+            #         float(t_width)/im_width,
+            #         float(t_height*1.15)/im_height]
 
-        # random noise
-        bcg_img = 0.1 * 255 * np.random.random(bcg_img.shape) + 0.9 * bcg_img
+            bbox = [float(x * random.uniform(0.996,1.01) + t_width*1.1/ 2) / im_width,
+                    float(y * random.uniform(0.998,1.02) + t_height * 1.15 / 2) / im_height,
+                    float(t_width * 1.20) / im_width,
+                    float(t_height * 1.20 ) / im_height]
 
-        bcg_img = np.array(bcg_img, dtype='uint8')
+            false_bbox = [min(abs(float(1 - x * random.uniform(0.7,1.5) + t_width*1.1/ 2) / im_width), 0.95),
+                    min(abs(float(1 - y * random.uniform(0.7,1.5) + t_height * 1.15 / 2) / im_height), 0.95),
+                    float(t_width * 1.20) / im_width,
+                    float(t_height * 1.20 ) / im_height]
 
-        # imsave('tc_tmp/%s_%s_%s.jpg' % (tc, x, y), bcg_img)
-        # imsave('tmp.jpg', bcg_img)
 
-        return [bcg_img, tc]
+            sigma = random.uniform(0, 0.9)
+            bcg_img = gaussian_filter(bcg_img, sigma)
+
+
+            bcg_img = np.array(bcg_img, dtype='uint8')
+
+
+
+            # imsave('tc_tmp/%s_%s_%s.jpg' % (tc, x, y), bcg_img)
+            # cv2.imwrite('res/%s_%s_%s.jpg' % (tc, x, y), bcg_img)
+
+            true_crop = cropBBox(bcg_img, bbox)
+            false_crop = cropBBox(bcg_img, false_bbox)
+
+            # cv2.imwrite('tmp/%s_%s_%s_true_crop.jpg' % (tc, x, y), true_crop)
+            # cv2.imwrite('tmp/%s_%s_%s_false_crop.jpg' % (tc, x, y), false_crop)
+            #cv2.imwrite('res/%s_%s_%s_bbox.jpg' % (tc, x, y), drawBBox(bbox, bcg_img))
+            # mask = np.packbits(np.asarray(mask, dtype='bool'), axis=-1)
+            return [true_crop,false_crop, tc]
 
 
 pool = ThreadPool(max(cpu_count() // 2, 4))
@@ -125,20 +169,30 @@ def getLabel(s):
 
 
 def gen(batch_size=32):
-    x = np.zeros((batch_size, im_height, im_width, 1), dtype=np.float)
-    y = np.zeros((batch_size, tc_len, len(vocabulary)))
-
+    x = np.zeros((batch_size, 27, 27*7, 3), dtype=np.float)
+    y = np.zeros((batch_size, 11, len(vocabulary)))
+    y_conf = np.zeros((batch_size, 1))
     while True:
-        result = pool.map(process, range(batch_size))
+        result = pool.map(process, range(batch_size/2))
+        k = 0
         for i, v in enumerate(result):
-            x[i, :, :, 0] = v[0] / 127.5 - 1
-            y[i] = getLabel(v[1])
+            x[k] = v[0].astype(np.float) / 127.5 - 1
+            y[k] = getLabel(v[2])
+            y_conf[k] = 1
+            k+=1
 
-        yield (x, y)
+            x[k] = v[1].astype(np.float) / 127.5 - 1
+            y[k] = 1./12.
+            y_conf[k] = 0
+            k+=1
 
-model = timecode_ocr_model_small()
-model.load_weights('checkpoints/OCRmodel_vl0.1142.hdf5')
+        yield (x, [y,y_conf])
+
+model = timecode_ocr_model_RGB_full()
+model.load_weights('checkpoints/timecode_ocr_model_RGB_full_vl0.0649.hdf5')
 model.summary()
+
+print(len(fonts), len(bcgs))
 
 model.fit_generator(generator=gen(batch_size=BATCH_SIZE),
                     validation_data=gen(batch_size=BATCH_SIZE),
@@ -148,6 +202,6 @@ model.fit_generator(generator=gen(batch_size=BATCH_SIZE),
                     max_q_size=100,
                     callbacks=[
                         callbacks.ModelCheckpoint(
-                            'checkpoints/OCRmodel_vl{val_loss:.4f}.hdf5',
+                            'checkpoints/timecode_ocr_model_RGB_full_vl{val_loss:.4f}.hdf5',
                             monitor='val_loss')
                     ])
